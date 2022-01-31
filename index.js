7aa51e30c153e0a9cfacfc4edda151475391ee91
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-31 16:55:35 +0000

    Require NotesModel in main file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// console.log doesn't print anything on the page
-// it is not meant to be visible to the user, but for you
-// to help in debugging and getting visibility in your JS code.
-//
-// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+const NotesModel = require('./notesModel');
 
-console.log('The notes app is running');
+let model = new NotesModel;
+
+console.log(model.getNotes());
